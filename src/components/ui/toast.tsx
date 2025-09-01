fe145fd17b05e18fbe52585eabf7ea46fdeee47d
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-01 12:24:13 +0530

    Add src/components/ui/toast.tsx

---
+++ src/components/ui/toast.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from 'react';
+
+export function ToastContainer() {
+  return <div className="fixed bottom-20 right-4 z-50"></div>;
+}
+
+export default ToastContainer;
